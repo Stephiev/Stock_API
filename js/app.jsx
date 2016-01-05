e644f1f016e30d8cba49c820405ee42c0ab89887
StephieV <StephieVictoria@gmail.com>
2016-01-05 11:02:12 -0800

    Minor additions

--- js/app.jsx
+++ js/app.jsx
@@ -1,16 +1,21 @@
 var React = require('react');
 var request = require('superagent');
-// var Header = require('./components/Header.jsx');
+var Header = require('./components/Header.jsx');
+var ReactDOM = require('react-dom');
 // var Pokedex = require('./components/Pokedex.jsx');
+
 
 var App = React.createClass({
   render: function() {
     return(
       <main>
-      
+      <Header />
       </main>
     )
   }
 })
 
-React.render(<App />, document.body)
+var element = React.createElement(App);
+
+
+ReactDOM.render(element, document.body)

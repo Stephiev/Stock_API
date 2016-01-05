49b6f0f539d352cc112ca031c943d768645a2694
StephieV <StephieVictoria@gmail.com>
2016-01-05 10:44:50 -0800

    Some more basic setup

---
+++ js/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var request = require('superagent');
+// var Header = require('./components/Header.jsx');
+// var Pokedex = require('./components/Pokedex.jsx');
+
+var App = React.createClass({
+  render: function() {
+    return(
+      <main>
+      
+      </main>
+    )
+  }
+})
+
+React.render(<App />, document.body)
